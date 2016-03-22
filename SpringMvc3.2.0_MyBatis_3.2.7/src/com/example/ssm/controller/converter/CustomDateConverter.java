2a0577ea99aa978071b468091dc801b11508157b
晓龙 <352977335@qq.com>
2016-03-22 17:54:54 +0800

    16,03,22,01
    自定义参数

---
+++ SpringMvc3.2.0_MyBatis_3.2.7/src/com/example/ssm/controller/converter/CustomDateConverter.java
@@ -0,0 +1,22 @@
+package com.example.ssm.controller.converter;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.core.convert.converter.Converter;
+
+public class CustomDateConverter implements Converter<String,Date>{
+
+	@Override
+	public Date convert(String arg0) {
+		try {
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
+			return simpleDateFormat.parse(arg0);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return null;	
+	}
+
+}
